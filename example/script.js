fdc608492e776f2c6bc42568b46e7b2a0f2a2caa
mofongo <mosiriveraserra@gmail.com>
2022-08-05 16:08:03 -0400

    Fixes: example

--- example/script.js
+++ example/script.js
@@ -1,14 +1,47 @@
 const body = document.getElementsByTagName('body')[0];
-const START = () => {
-    for (let i = 5; i--;)
+const obstacles = [
     {
-        let box = document.createElement('div');
-        box.className = 'obstacle box';
-        box.style.left = Math.random(0) * window.innerWidth + 'px';
-        box.style.top = 440 + 'px';
-        box.innerHTML = 'Div';
-        box.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        body.appendChild(box);
+        type: 'input',
+        left: '20vw',
+        top: '85vh',
+        class: 'obstacle'
+    },
+    {
+        type: 'input',
+        left: '30vw',
+        top: '81vh',
+        class: 'obstacle'
+    },
+    {
+        type: 'div',
+        left: '43vw',
+        top: '77vh',
+        class: 'obstacle box'
+    },
+    {
+        type: 'div',
+        left: '50vw',
+        top: '80vh',
+        class: 'obstacle box'
+    },
+    {
+        type: 'div',
+        left: '55vw',
+        top: '85vh',
+        class: 'obstacle box'
+    }
+];
+const START = () => {
+    for (let obs of obstacles)
+    {
+        let obstacle_element = document.createElement(obs.type);
+        obstacle_element.className = obs.class;
+        obstacle_element.style.left =obs.left;
+        obstacle_element.style.top = obs.top;
+        obstacle_element.innerHTML = obs.type;
+        obstacle_element.placeholder = obs.type;
+        obstacle_element.style.backgroundColor = obs.color || '#' + Math.floor(Math.random()*16777215).toString(16);
+        document.body.appendChild(obstacle_element);
     }
     const options = {
         collidableClass: 'obstacle'
